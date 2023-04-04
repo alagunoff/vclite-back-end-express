0166eb34eace93abf8669531005f0f465b0bcc72
alagunoff <lagunov.artamon@gmail.com>
2023-04-04 10:49:03 +0300

    added controllers to update and delete tags

--- src/controllers/tags.js
+++ src/controllers/tags.js
@@ -16,11 +16,12 @@ async function createTag(req, res) {
 }
 
 async function getTags(req, res) {
+  const { limit, offset } = createPaginationParameters(
+    req.query.items_number,
+    req.query.page_number
+  );
+
   try {
-    const { limit, offset } = createPaginationParameters(
-      req.query.items_number,
-      req.query.page_number
-    );
     const { rows, count } = await Tag.findAndCountAll({ limit, offset });
 
     res.json(createPaginatedResponse(rows, count, limit));
@@ -29,7 +30,41 @@ async function getTags(req, res) {
   }
 }
 
+async function updateTag(req, res) {
+  const tagToUpdate = await Tag.findByPk(req.params.id);
+
+  if (tagToUpdate) {
+    try {
+      await tagToUpdate.update({ tag: req.body.tag });
+
+      res.status(204).end();
+    } catch (error) {
+      res.status(400).json(createErrorsObject(error));
+    }
+  } else {
+    res.status(404).end();
+  }
+}
+
+async function deleteTag(req, res) {
+  const tagToDelete = await Tag.findByPk(req.params.id);
+
+  if (tagToDelete) {
+    try {
+      await tagToDelete.destroy();
+
+      res.status(204).end();
+    } catch (error) {
+      res.status(400).json(createErrorsObject(error));
+    }
+  } else {
+    res.status(404).end();
+  }
+}
+
 module.exports = {
   createTag,
   getTags,
+  updateTag,
+  deleteTag,
 };
--- src/routers/tags.js
+++ src/routers/tags.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { createTag, getTags } = require("../controllers/tags");
+const {
+  createTag,
+  getTags,
+  updateTag,
+  deleteTag,
+} = require("../controllers/tags");
 const authenticateUser = require("../middlewares/authenticateUser");
 const isAdmin = require("../middlewares/isAdmin");
 
 router.post("", [authenticateUser(404), isAdmin], createTag);
 router.get("", getTags);
+router.patch("/:id", [authenticateUser(404), isAdmin], updateTag);
+router.delete("/:id", [authenticateUser(404), isAdmin], deleteTag);
 
 module.exports = router;
