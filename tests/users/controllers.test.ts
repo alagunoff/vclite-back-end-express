9a90bb6ed4ee63e74ec296eea19cc08d84c1992d
alagunoff <lagunov.artamon@yandex.ru>
2023-06-30 18:19:14 +0300

    refactored tests

--- tests/users/controllers.test.ts
+++ tests/users/controllers.test.ts
@@ -1,4 +1,5 @@
 import { jest, describe, test, expect, afterEach } from "@jest/globals";
+import { getMockReq, getMockRes } from "@jest-mock/express";
 
 import { createUser } from "collections/users/controllers";
 import * as services from "collections/users/services";
@@ -8,38 +9,32 @@ import { ApiError } from "shared/errors/classes";
 jest.mock("collections/users/validators");
 jest.mock("collections/users/services");
 
-const mockReq: any = {};
-const mockRes: any = {};
-mockRes.json = jest.fn(() => mockRes);
-mockRes.end = jest.fn(() => mockRes);
-mockRes.status = jest.fn(() => mockRes);
+const { res: mockRes, mockClear } = getMockRes();
 const mockValidators = jest.mocked(validators);
 const mockServices = jest.mocked(services);
 
 afterEach(() => {
   jest.restoreAllMocks();
-  mockRes.json.mockRestore();
-  mockRes.end.mockRestore();
-  mockRes.status.mockRestore();
+  mockClear();
 });
 
 describe("createUser", () => {
   test("should set response status 400 when there are creation data validation errors", async () => {
     mockValidators.validateCreationData.mockReturnValue({ image: "required" });
-    await createUser(mockReq, mockRes);
+    await createUser(getMockReq(), mockRes);
 
     expect(mockRes.status).toBeCalledWith(400);
   });
 
   test("should set response status 422 when there is user's creation error", async () => {
     mockServices.createUser.mockResolvedValue(new ApiError(422));
-    await createUser(mockReq, mockRes);
+    await createUser(getMockReq(), mockRes);
 
     expect(mockRes.status).toBeCalledWith(422);
   });
 
   test("should set response status 201 when user has been created", async () => {
-    await createUser(mockReq, mockRes);
+    await createUser(getMockReq(), mockRes);
 
     expect(mockRes.status).toBeCalledWith(201);
   });
